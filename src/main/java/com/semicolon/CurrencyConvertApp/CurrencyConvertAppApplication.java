e1593ec152a9b6a24841e4105b4a71d8b6252f17
Flosamuel <nwabuezesamuel21@gmail.com>
2025-02-11 12:49:10 +0100

    first commit

---
+++ src/main/java/com/semicolon/CurrencyConvertApp/CurrencyConvertAppApplication.java
@@ -0,0 +1,13 @@
+package com.semicolon.CurrencyConvertApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConvertAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConvertAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semicolon/CurrencyConvertApp/CurrencyConvertAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.CurrencyConvertApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConvertAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
